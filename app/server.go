abb4c89abce85a4d0f4fa68c10c10bb77ec7fad7
khongtrunght <khongtrunght@gmail.com>
2024-06-12 17:20:42 +0700

    pass stage

--- app/server.go
+++ app/server.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+const CRLF = "\r\n"
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -18,12 +20,33 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go func(conn net.Conn) {
+			defer conn.Close()
+			buf := make([]byte, 1024)
+			n, err := conn.Read(buf)
+			if err != nil {
+				fmt.Printf("Error reading from connection: %v", err)
+				return
+			}
+
+			// get resourse path
+			var path string
+			fmt.Sscanf(string(buf[:n]), "GET %s HTTP/1.1", &path)
+			if path == "/" {
+				conn.Write([]byte("HTTP/1.1 200 OK" + CRLF + CRLF))
+			} else {
+				// 404
+				conn.Write([]byte("HTTP/1.1 404 Not Found" + CRLF + CRLF))
+			}
+		}(conn)
 	}
-	const CRLF = "\r\n"
-	conn.Write([]byte("HTTP/1.1 200 OK" + CRLF + CRLF))
 }
